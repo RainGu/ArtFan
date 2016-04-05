0c6e042b47617c410f68237e549b5dbc608439ba
RainGu <RainGu@fengyangdeiMac.local>
2016-04-05 22:38:39 +0800

    Begin Art Fans Project

---
+++ ArtFans_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtFans_Swift
+//
+//  Created by RainGu on 16/4/5.
+//  Copyright © 2016年 RainGu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
